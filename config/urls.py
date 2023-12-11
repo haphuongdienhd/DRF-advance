60e7722ce0a6ed4174be6efbacc5539684f2a0ae
haphuongdienhd <91938416+haphuongdien@users.noreply.github.com>
2023-12-11 14:01:56 +0700

    add swagger

--- config/urls.py
+++ config/urls.py
@@ -13,11 +13,33 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import os, environ
 from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Posts API",
+        default_version='1.0.0',
+        description="API documentation of App",
+    ),
+    public=True,
+)
+
+
+
+
 urlpatterns = [
+    path(
+        "docs/",
+        schema_view.with_ui('swagger', cache_timeout=0),
+        name="swagger-schema"
+    ),
     path('admin/', admin.site.urls),
     path("v1/", include("config.api")),
 ]
